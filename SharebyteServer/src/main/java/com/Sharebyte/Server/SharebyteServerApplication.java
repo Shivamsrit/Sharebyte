d21c6a071126b49ab4a639ee02b0a1f5f232f7ae
Shivam Jaiswal <shivamsrit980@gmail.com>
2025-09-27 23:47:32 +0530

    intial commit

---
+++ SharebyteServer/src/main/java/com/Sharebyte/Server/SharebyteServerApplication.java
@@ -0,0 +1,13 @@
+package com.Sharebyte.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebyteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharebyteServerApplication.class, args);
+	}
+
+}
---
+++ SharebyteServer/src/test/java/com/Sharebyte/Server/SharebyteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sharebyte.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebyteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
